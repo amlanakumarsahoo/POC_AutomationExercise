69e0923eb0e00c9d74a2fdd91d5d7f283a4667e3
Amlana Kumar Sahoo <amlana.ksahoo@gmail.com>
2026-01-20 20:26:13 +0530

    removed duplicate cucumber configuration

--- cucumber.js
+++ cucumber.js
@@ -1,23 +1,48 @@
+const commonConfig = {
+  requireModule: ['ts-node/register', 'tsconfig-paths/register'],
+  require: ['src/test/steps/**/*.ts', 'src/main/hooks.ts'],
+  format: [
+    'progress',
+    'html:results/playwright-report/cucumber-html-report.html',
+    'json:results/playwright-report/cucumber-report.json'
+  ],
+  formatOptions: {
+    snippetInterface: 'async-await'
+  },
+  publishQuiet: true,
+  timeout: 60000 // 60 seconds per step
+};
+
 module.exports = {
-    default: {
-      requireModule: [
-        "ts-node/register",
-        "tsconfig-paths/register"
-      ],
-      require: [
-        "src/test/steps/**/*.ts",
-        "src/main/hooks.ts"
-      ],
-      format: [
-        'progress',
-        'html:results/playwright-report/cucumber-html-report.html',
-        'json:results/playwright-report/cucumber-report.json'
-      ],
-      formatOptions: {
-        snippetInterface: 'async-await'
-      },
-      publishQuiet: true,
-      // Global timeout settings
-      timeout: 60000 // 60 seconds for each step
+  default: {
+    ...commonConfig,
+    worldParameters: {
+      baseUrl: 'https://automationexercise.com/',
+      environment: 'default'
     }
-  };
+  },
+
+  test: {
+    ...commonConfig,
+    worldParameters: {
+      baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',
+      environment: 'test'
+    }
+  },
+
+  stage: {
+    ...commonConfig,
+    worldParameters: {
+      baseUrl: 'https://stage.orangehrmlive.com',
+      environment: 'stage'
+    }
+  },
+
+  prod: {
+    ...commonConfig,
+    worldParameters: {
+      baseUrl: 'https://orangehrmlive.com',
+      environment: 'prod'
+    }
+  }
+};
